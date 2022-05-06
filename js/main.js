0e556df10513eb448971cf460d9d73bdf099e1eb
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-05-06 14:03:37 +0530

    js: loop through array and add details to html

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // testing JS wiring
 // console.log('wired');
+let parent = document.querySelector('.book');
 
 // book constructor
 function bookMaker(name, author, numberOfPages, readStatus) {
@@ -13,6 +14,44 @@ function bookMaker(name, author, numberOfPages, readStatus) {
         this.getInfo = function () { return console.log(this.info()) }
 }
 
-const potterHead = new bookMaker('goblet of fire', 'j k rowling', 400, 'read');
 
-// potterHead.getInfo();
+
+// array of books and a function to add to that array
+let library = [];
+const addToLibrary = (book) => library.push(book);
+
+// random books to test thing out
+const potterHead = new bookMaker('goblet of fire', 'j k rowling', 400, 'read');
+addToLibrary(potterHead);
+const winner = new bookMaker('you can win', 'Shiv Khera', 183, 'not read');
+addToLibrary(winner);
+
+
+// looping through the array
+library.forEach(item => {
+
+        // creating an element for each book 'item'
+        let newBook = document.createElement('div');
+
+        // creating elments for book's properties
+        let newName = document.createElement('div');
+        let newAuthor = document.createElement('div');
+        let newPages = document.createElement('div');
+        let newReadStatus = document.createElement('div');
+
+        // updating properties of the book
+        newName.innerText = item.name;
+        newAuthor.innerText = item.author;
+        newPages.innerText = item.numberOfPages;
+        newReadStatus.innerText = item.readStatus;
+
+        // adding new property divs to the parent newBook node
+        newBook.appendChild(newName);
+        newBook.appendChild(newAuthor);
+        newBook.appendChild(newPages);
+        newBook.appendChild(newReadStatus);
+
+
+        // adding 'newBook' as a child to the 'book' div in HTML
+        parent.appendChild(newBook);
+})
